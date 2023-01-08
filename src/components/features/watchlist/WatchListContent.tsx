e056b1c1c15ab1f38cb1692eb59b277d02676f0f
Julien Zapata Duque <jzapata@live.nl>
2023-01-08 16:55:48 +0100

    perf: don't rerender all items when changing seen status

--- src/components/features/watchlist/WatchListContent.tsx
+++ src/components/features/watchlist/WatchListContent.tsx
@@ -1,9 +1,9 @@
-import React, { Fragment, useCallback } from 'react';
-import type { Movie as PrismaMovie, MoviesOnWatchlists } from '@prisma/client';
-import { Rating } from '~/components/ui/Rating';
-import { SeenBadge } from '~/components/ui/SeenBadge';
 import { Menu, Transition } from '@headlessui/react';
 import { EllipsisVerticalIcon, EyeIcon, EyeSlashIcon, TrashIcon } from '@heroicons/react/20/solid';
+import type { Movie as PrismaMovie, MoviesOnWatchlists } from '@prisma/client';
+import React, { Fragment, memo, useCallback } from 'react';
+import { Rating } from '~/components/ui/Rating';
+import { SeenBadge } from '~/components/ui/SeenBadge';
 import { trpc } from '~/utils/trpc';
 
 type WatchlistItem = Omit<PrismaMovie, 'rating' | 'source'> &
@@ -16,95 +16,125 @@ interface WatchlistContentProps {
   watchlistId: string;
 }
 
-const WatchlistContextMenu: React.FC<{ item: WatchlistItem & Pick<MoviesOnWatchlists, 'seenOn' | 'watchlistId'> }> = ({
-  item,
-}) => {
-  const trpcUtil = trpc.useContext();
+// Component is memoized to prevent re-rendering every item when one item is updated
+const WatchlistContextMenu: React.FC<{ item: WatchlistItem & Pick<MoviesOnWatchlists, 'seenOn' | 'watchlistId'> }> =
+  memo(
+    ({ item }) => {
+      const trpcUtil = trpc.useContext();
 
-  const editWatchlistItem = trpc.watchlist.editItem.useMutation({
-    onSuccess: async () => {
-      await trpcUtil.watchlist.byId.invalidate({ id: item.watchlistId });
-    },
-  });
-  const removeWatchlistItem = trpc.watchlist.removeItem.useMutation({
-    onSuccess: async () => {
-      await trpcUtil.watchlist.byId.invalidate({ id: item.watchlistId });
-    },
-  });
+      const editWatchlistItem = trpc.watchlist.editItem.useMutation({
+        onMutate: (newItem) => {
+          const previousData = trpcUtil.watchlist.byId.getData({ id: newItem.watchlistId });
+          if (!previousData) return previousData;
 
-  const handleChangeSeen = useCallback(() => {
-    editWatchlistItem.mutate({
-      id: item.id,
-      watchlistId: item.watchlistId,
-      seenOn: item.seenOn ? null : new Date(),
-    });
-  }, [editWatchlistItem, item]);
+          trpcUtil.watchlist.byId.setData(
+            {
+              id: newItem.watchlistId,
+            },
+            {
+              ...previousData,
+              movies: previousData.movies.map((movie) => {
+                if (movie.id === newItem.id) {
+                  return {
+                    ...movie,
+                    seenOn: newItem.seenOn,
+                  };
+                }
+                return movie;
+              }),
+            }
+          );
+          return { previousData };
+        },
+        onError: (_, newItem, context) => {
+          if (context?.previousData) {
+            trpcUtil.watchlist.byId.setData({ id: newItem.watchlistId }, context.previousData);
+          }
+        },
+      });
 
-  const handleRemoveItem = useCallback(() => {
-    removeWatchlistItem.mutate({
-      id: item.id,
-      watchlistId: item.watchlistId,
-    });
-  }, [removeWatchlistItem, item]);
+      const removeWatchlistItem = trpc.watchlist.removeItem.useMutation({
+        onSuccess: async () => {
+          await trpcUtil.watchlist.byId.invalidate({ id: item.watchlistId });
+        },
+      });
 
-  return (
-    <Menu>
-      <Menu.Button className="p-0.5 hover:rounded hover:bg-base-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-base-100 focus-visible:ring-opacity-75">
-        <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
-      </Menu.Button>
+      const handleChangeSeen = useCallback(() => {
+        editWatchlistItem.mutate({
+          id: item.id,
+          watchlistId: item.watchlistId,
+          seenOn: item.seenOn ? null : new Date(),
+        });
+      }, [editWatchlistItem, item]);
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-base-200 rounded-md bg-base-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <Menu.Item>
-            {({ active }) => (
-              <button
-                onClick={handleChangeSeen}
-                className={`${
-                  active ? 'bg-base-200 text-base-content' : 'text-base-content'
-                } group flex w-full items-center gap-x-2 rounded-md px-2 py-2 text-sm`}
-              >
-                {item.seenOn ? (
-                  <>
-                    <EyeSlashIcon className="h-5 w-5" />
-                    <span>Mark as not seen</span>
-                  </>
-                ) : (
-                  <>
-                    <EyeIcon className="h-5 w-5" />
-                    <span>Mark as seen</span>
-                  </>
+      const handleRemoveItem = useCallback(() => {
+        removeWatchlistItem.mutate({
+          id: item.id,
+          watchlistId: item.watchlistId,
+        });
+      }, [removeWatchlistItem, item]);
+
+      return (
+        <Menu>
+          <Menu.Button className="p-0.5 hover:rounded hover:bg-base-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-base-100 focus-visible:ring-opacity-75">
+            <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+          </Menu.Button>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 mt-2 w-44 origin-top-right divide-y divide-base-200 rounded-md bg-base-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={handleChangeSeen}
+                    className={`${
+                      active ? 'bg-base-200 text-base-content' : 'text-base-content'
+                    } group flex w-full items-center gap-x-2 rounded-md px-2 py-2 text-sm`}
+                  >
+                    {item.seenOn ? (
+                      <>
+                        <EyeSlashIcon className="h-5 w-5" />
+                        <span>Mark as not seen</span>
+                      </>
+                    ) : (
+                      <>
+                        <EyeIcon className="h-5 w-5" />
+                        <span>Mark as seen</span>
+                      </>
+                    )}
+                  </button>
                 )}
-              </button>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <button
-                onClick={handleRemoveItem}
-                className={`${
-                  active ? 'bg-base-200 text-base-content' : 'text-base-content'
-                } group flex w-full items-center gap-x-2 rounded-md px-2 py-2 text-sm`}
-              >
-                <>
-                  <TrashIcon className="h-5 w-5 text-error" />
-                  <span>Remove</span>
-                </>
-              </button>
-            )}
-          </Menu.Item>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={handleRemoveItem}
+                    className={`${
+                      active ? 'bg-base-200 text-base-content' : 'text-base-content'
+                    } group flex w-full items-center gap-x-2 rounded-md px-2 py-2 text-sm`}
+                  >
+                    <>
+                      <TrashIcon className="h-5 w-5 text-error" />
+                      <span>Remove</span>
+                    </>
+                  </button>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      );
+    },
+    (prev, next) => prev.item.id === next.item.id && prev.item.seenOn === next.item.seenOn
   );
-};
+WatchlistContextMenu.displayName = 'WatchlistContextMenu';
 
 export const WatchlistContent: React.FC<WatchlistContentProps> = ({ items, watchlistId }) => {
   return (
