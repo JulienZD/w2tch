f3306eabcc67473aceaf1e849b45f48b22a17b2d
Julien Zapata Duque <jzapata@live.nl>
2023-01-08 16:20:44 +0100

    fixup autocomplete

--- src/components/input/Autocomplete.tsx
+++ src/components/input/Autocomplete.tsx
@@ -70,49 +70,51 @@ export function Autocomplete<T>({
   const Value = RenderValue || DefaultRenderValue;
 
   return (
-    <Combobox value={selected} onChange={handleSelect}>
-      <div className="relative mt-1">
-        <div className="relative w-full cursor-default overflow-hidden rounded-lg text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-          <Combobox.Input
-            placeholder={placeholder}
-            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-base-content focus:ring-0"
-            displayValue={(result: SelectItem<T> | undefined) => result?.name ?? ''}
-            onChange={(event) => handleSearch(event.target.value)}
-          />
-          {openDropdownButton && (
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronUpDownIcon className="h-5 w-5 text-base-content" aria-hidden="true" />
-            </Combobox.Button>
-          )}
-        </div>
-        <Transition
-          as={Fragment}
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          afterLeave={() => setQuery('')}
-        >
-          <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-base-100 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {items.length === 0 && query !== '' ? (
-              <div className="relative cursor-default select-none py-2 px-4 text-base-content">Nothing found.</div>
-            ) : (
-              items.map((result) => (
-                <Combobox.Option
-                  key={result.id}
-                  className={({ active }) =>
-                    `relative cursor-pointer select-none py-2 px-4 ${
-                      active ? 'bg-teal-600 text-white' : 'text-base-content'
-                    }`
-                  }
-                  value={result}
-                >
-                  {(options) => <Value item={result} renderOptions={options} />}
-                </Combobox.Option>
-              ))
+    <div className="z-50">
+      <Combobox value={selected} onChange={handleSelect}>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              placeholder={placeholder}
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-base-content focus:ring-0"
+              displayValue={(result: SelectItem<T> | undefined) => result?.name ?? ''}
+              onChange={(event) => handleSearch(event.target.value)}
+            />
+            {openDropdownButton && (
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon className="h-5 w-5 text-base-content" aria-hidden="true" />
+              </Combobox.Button>
             )}
-          </Combobox.Options>
-        </Transition>
-      </div>
-    </Combobox>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-base-100 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {items.length === 0 && query !== '' ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-base-content">Nothing found.</div>
+              ) : (
+                items.map((result) => (
+                  <Combobox.Option
+                    key={result.id}
+                    className={({ active }) =>
+                      `relative cursor-pointer select-none py-2 px-4 ${
+                        active ? 'bg-teal-600 text-white' : 'text-base-content'
+                      }`
+                    }
+                    value={result}
+                  >
+                    {(options) => <Value item={result} renderOptions={options} />}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
   );
 }
