36b79840e734802c58e6a65f274f27761098cfa8
Julien Zapata Duque <jzapata@live.nl>
2023-01-15 20:16:25 +0100

    fix: align runtime icons

--- src/components/ui/Rating.tsx
+++ src/components/ui/Rating.tsx
@@ -2,10 +2,11 @@ import { StarIcon } from '@heroicons/react/20/solid';
 
 type RatingProps = {
   score: number;
+  className?: string;
 };
 
-export const Rating: React.FC<RatingProps> = ({ score }) => (
-  <span className="flex select-none items-center gap-x-1">
+export const Rating: React.FC<RatingProps> = ({ score, className = '' }) => (
+  <span className={`flex select-none items-center gap-x-1 ${className}`}>
     <StarIcon className="h-4 w-4 fill-yellow-500" />
     {score}
   </span>
