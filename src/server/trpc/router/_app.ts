3b934320d99eb4f1a5e2720a1191698dd63dcfc6
Julien Zapata Duque <jzapata@live.nl>
2023-01-16 18:46:21 +0100

    refactor: remove example api routes

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,12 +1,10 @@
 import { router } from '../trpc';
 import { authRouter } from './auth';
-import { exampleRouter } from './example';
 import { searchRouter } from './search';
 import { watchlistRouter } from './watchlist';
 import { inviteRouter } from './invite';
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   watchlist: watchlistRouter,
   invite: inviteRouter,
