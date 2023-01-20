5d07f4a34c19065a10f841c0a879a4f5da61c8f9
Julien Zapata Duque <jzapata@live.nl>
2023-01-20 13:24:58 +0100

    fix: use correct base url in prod / preview

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -5,9 +5,11 @@ import superjson from 'superjson';
 
 import { type AppRouter } from '../server/trpc/router/_app';
 
+const publicUrl = process.env.NEXT_PUBLIC_URL ?? process.env.VERCEL_URL;
+
 export const getBaseUrl = () => {
   if (typeof window !== 'undefined') return ''; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (publicUrl) return `https://${publicUrl}`; // SSR should use public url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
