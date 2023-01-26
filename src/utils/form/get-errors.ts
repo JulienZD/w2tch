de12285c66c56f58f0f392a820e91fcc2741d7fc
Julien Zapata Duque <jzapata@live.nl>
2023-01-26 21:11:28 +0100

    feat: construct strongly typed error objects

--- src/utils/form/get-errors.ts
+++ src/utils/form/get-errors.ts
@@ -2,8 +2,13 @@ import type { UseTRPCMutationResult } from '@trpc/react-query/shared';
 import type { FieldErrorsImpl } from 'react-hook-form';
 import type { z } from 'zod';
 
+type ErrorKey<TFormErrors extends FormErrors, TMutation extends TRPCMutationWithZodError> = keyof NonNullable<
+  TMutation['variables']
+> &
+  keyof TFormErrors;
+
 export const getFormOrMutationError = <TFormErrors extends FormErrors, TMutation extends TRPCMutationWithZodError>(
-  field: keyof NonNullable<TMutation['variables']> & keyof TFormErrors,
+  field: ErrorKey<TFormErrors, TMutation>,
   errors: TFormErrors,
   mutation?: TMutation
 ) => {
@@ -11,11 +16,26 @@ export const getFormOrMutationError = <TFormErrors extends FormErrors, TMutation
   return errors[_field]?.message ?? mutation?.error?.data?.zodError?.fieldErrors?.[_field];
 };
 
-type FormErrors = Partial<
-  FieldErrorsImpl<{
-    [x: string]: unknown;
-  }>
->;
+export const getFormOrMutationErrors = <TFormErrors extends FormErrors, TMutation extends TRPCMutationWithZodError>(
+  errors: TFormErrors,
+  mutation?: TMutation
+): Record<ErrorKey<TFormErrors, TMutation>, string | string[] | undefined> => {
+  type _ErrorKey = ErrorKey<TFormErrors, TMutation>;
+  const _errors = {} as Record<_ErrorKey, string | string[] | undefined>;
+  Object.keys(errors).forEach((key) => {
+    _errors[key as _ErrorKey] = getFormOrMutationError(key as _ErrorKey, errors, mutation);
+  });
+
+  if (!mutation?.error?.data?.zodError?.fieldErrors) return _errors;
+
+  Object.keys(mutation.error.data.zodError.fieldErrors).forEach((key) => {
+    _errors[key as _ErrorKey] = getFormOrMutationError(key as _ErrorKey, errors, mutation);
+  });
+
+  return _errors;
+};
+
+type FormErrors = Partial<FieldErrorsImpl<Record<string, unknown>>>;
 
 type TRPCMutationWithZodError = UseTRPCMutationResult<
   unknown,
