90e172a4def52617ee210b046da39f7b980e34e8
Julien Zapata Duque <jzapata@live.nl>
2023-01-28 23:09:49 +0100

    feat: configure sentry for production

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -10,12 +10,15 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 if (process.env.NODE_ENV !== 'development' && SENTRY_DSN) {
   Sentry.init({
     dsn: SENTRY_DSN,
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1.0,
-    // ...
-    // Note: if you want to override the automatic release value, do not set a
-    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-    // that it will also get attached to your source maps
+    tracesSampleRate: 0.2,
+    tracesSampler: (samplingContext) => {
+      // Ignore auth session requests
+      if (samplingContext?.request?.url === '/api/auth/session') {
+        return 0;
+      }
+
+      return 0.2;
+    },
     integrations: [new ExtraErrorDataIntegration()],
   });
 }
