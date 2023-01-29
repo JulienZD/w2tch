0d08231a8a40e3fb89e14091cc61d746c305aebb
Julien Zapata Duque <jzapata@live.nl>
2023-01-29 13:24:11 +0100

    feat: navigation progress indicator
    Closes #30

---
+++ src/components/ui/NavigationProgress.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router';
+
+import NProgress from 'nprogress';
+import { useEffect } from 'react';
+
+NProgress.configure({
+  showSpinner: false,
+  template:
+    '<div class="fixed top-0 left-0 right-0 z-10"><div class="h-1 bg-blue-500" role="bar" style="width: 100%;"><div class="peg"></div></div></div>',
+});
+
+export const NavigationProgress: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (url !== router.asPath) {
+        NProgress.start();
+      }
+    };
+    const handleRouteChangeComplete = () => NProgress.done();
+    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+    router.events.on('routeChangeError', handleRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+      router.events.off('routeChangeError', handleRouteChangeComplete);
+    };
+  }, [router]);
+
+  return null;
+};
