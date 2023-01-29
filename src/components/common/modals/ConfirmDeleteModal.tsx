2d4ffdf74c91b52a69d0180902c23f9a9ada631b
Julien Zapata Duque <jzapata@live.nl>
2023-01-30 00:09:32 +0100

    refactor: remove account deletion modal
    The new confirm delete modal provides the same functionality

--- src/components/common/modals/ConfirmDeleteModal.tsx
+++ src/components/common/modals/ConfirmDeleteModal.tsx
@@ -4,19 +4,22 @@ import { Modal, type ModalProps } from '~/components/ui/Modal';
 type DeleteModalProps = Pick<ModalProps, 'onCancel' | 'title'> & {
   deleteFn: () => void | Promise<void>;
   message?: string;
+  buttonLabel?: string;
 };
 
-export const ConfirmDeleteModal = NiceModal.create<DeleteModalProps>(({ deleteFn, title, message, onCancel }) => (
-  <Modal
-    title={title}
-    primaryBtnColor="danger"
-    primaryBtnLabel="Delete"
-    primaryBtnOnClick={deleteFn}
-    onCancel={onCancel}
-    description={message}
-  >
-    <p className="mt-2">
-      This action <span className="font-bold">cannot</span> be undone.
-    </p>
-  </Modal>
-));
+export const ConfirmDeleteModal = NiceModal.create<DeleteModalProps>(
+  ({ deleteFn, title, message, onCancel, buttonLabel = 'Delete' }) => (
+    <Modal
+      title={title}
+      primaryBtnColor="danger"
+      primaryBtnLabel={buttonLabel}
+      primaryBtnOnClick={deleteFn}
+      onCancel={onCancel}
+      description={message}
+    >
+      <p className="mt-2">
+        This action <span className="font-bold">cannot</span> be undone.
+      </p>
+    </Modal>
+  )
+);
