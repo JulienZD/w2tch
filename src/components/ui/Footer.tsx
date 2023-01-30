f061e57bc067257172e10e5bb9b347a0a54341a5
Julien Zapata Duque <jzapata@live.nl>
2023-01-30 02:14:33 +0100

    fix: revert order of footer items

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -5,16 +5,16 @@ export const Footer: React.FC<{ fixed?: boolean }> = ({ fixed }) => {
     <footer className={fixed ? 'fixed bottom-2 mx-auto' : 'mt-4 pb-2'}>
       <ul className="flex w-full items-center gap-x-4 text-sm">
         <li>
-          Data provided by{' '}
-          <Link href="/about" className="text-primary">
-            TMDB
+          Made by{' '}
+          <Link href="https://github.com/JulienZD/" className="text-primary" rel="noreferrer" target="_blank">
+            Julien
           </Link>
         </li>
 
         <li>
-          Made by{' '}
-          <Link href="https://github.com/JulienZD/" className="text-primary" rel="noreferrer" target="_blank">
-            Julien
+          Data provided by{' '}
+          <Link href="/about" className="text-primary">
+            TMDB
           </Link>
         </li>
       </ul>
