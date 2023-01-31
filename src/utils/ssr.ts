fdf23ecc30625ee65c00697ff806e91b57d4a9f0
Julien Zapata Duque <jzapata@live.nl>
2023-01-31 21:58:00 +0100

    docs: add link to discussion regarding getServerSideProps

--- src/utils/ssr.ts
+++ src/utils/ssr.ts
@@ -4,6 +4,8 @@ import type { IncomingMessage } from 'http';
  * Whether the request is a client navigation request
  *
  * This can be used to determine whether to continue with performing operations in getServerSideProps
+ *
+ * https://github.com/vercel/next.js/discussions/19611#discussioncomment-972107
  */
 export const isLoadedViaClientNavigation = (req?: IncomingMessage) => {
   return req?.url?.indexOf('/_next/data/') !== -1;
