42f3891633b5fe7f099e4c71769daf91d7b01aa9
Julien Zapata Duque <jzapata@live.nl>
2023-01-31 11:03:07 +0100

    feat: support objects in query params

--- src/hooks/useQueryParam.ts
+++ src/hooks/useQueryParam.ts
@@ -1,17 +1,38 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { z } from 'zod';
 
-export const useQueryParam = <T extends string | undefined>(key: string, defaultValue?: T): [T, (value: T) => void] => {
+const isPrimitive = (value: unknown): value is string | number | boolean | undefined => {
+  return value === null || value === undefined || (typeof value !== 'object' && typeof value !== 'function');
+};
+
+const zPrimitive = z.union([z.string(), z.number(), z.boolean()]);
+const zStringifiedJson = z.preprocess((v) => {
+  if (typeof v !== 'string') return v;
+  try {
+    return JSON.parse(v);
+  } catch (err) {
+    return v;
+  }
+}, z.record(zPrimitive));
+const zQueryParam = z.union([zStringifiedJson, zPrimitive.array(), zPrimitive]).optional();
+
+export const useQueryParam = <T extends string | Record<string, unknown> | boolean | number | undefined>(
+  key: string,
+  defaultValue?: T
+): [T, (value: T) => void] => {
   const router = useRouter();
 
   const [value, setValue] = useState<T>((): T => {
     const value = router.query[key];
 
-    if (Array.isArray(value) && !!value.length) {
-      return value[0] as T;
+    const parseResult = zQueryParam.safeParse(value);
+    if (!parseResult.success) {
+      console.warn(`Invalid query param for key ${key}: ${String(value)}`);
+      return defaultValue as T;
     }
 
-    return (value ?? defaultValue) as T;
+    return parseResult.data as T;
   });
 
   const updateQueryParam = (value: T | undefined) => {
@@ -21,7 +42,7 @@ export const useQueryParam = <T extends string | undefined>(key: string, default
           {
             query: {
               ...router.query,
-              [key]: value,
+              [key]: isPrimitive(value) ? value : JSON.stringify(value),
             },
           },
           undefined,
