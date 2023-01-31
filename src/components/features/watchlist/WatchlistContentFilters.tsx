43694a008fb00ac13676b294426de0c6b404d206
Julien Zapata Duque <jzapata@live.nl>
2023-01-31 14:50:57 +0100

    fix: responsive filter layout

--- src/components/features/watchlist/WatchlistContentFilters.tsx
+++ src/components/features/watchlist/WatchlistContentFilters.tsx
@@ -1,6 +1,7 @@
 import { Menu } from '@headlessui/react';
 import { FunnelIcon, ArrowsUpDownIcon, EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
 import { OverflowMenu } from '~/components/ui/OverflowMenu';
+import { useBreakpoint } from '~/hooks/useTWBreakpoint';
 import type { useFilterWatchlistEntries } from '~/hooks/watchlist/useFilterWatchlistEntries';
 
 type FilterArgs = Omit<ReturnType<typeof useFilterWatchlistEntries>, 'filteredWatchables'>;
@@ -13,19 +14,20 @@ const sortedByTextMap = {
   'name.desc': 'Title (Z-A)',
   'seenAt.desc': 'Seen',
   'seenAt.asc': 'Not seen',
-  'rating.asc': 'Rating (Lowest)',
-  'rating.desc': 'Rating (Highest)',
+  'rating.asc': 'Lowest Rating',
+  'rating.desc': 'Highest Rating',
 } satisfies Record<SortKey, string>;
 
 const getSortedByText = (sort: SortFilters | undefined, defaultValue: string) => {
   if (!sort) return defaultValue;
 
   const category = sortedByTextMap[`${sort.by}.${sort.order}`];
-  return category ? `Sorted by ${category}` : defaultValue;
+  return category ?? defaultValue;
 };
 
 export const WatchlistContentFilters: React.FC<{ filters: FilterArgs }> = ({ filters }) => {
   const sortedByButtonText = getSortedByText(filters.sort, 'Sort');
+  const { isMd } = useBreakpoint('md');
 
   return (
     <div className="mt-4 flex flex-row items-center gap-x-2">
@@ -103,8 +105,8 @@ export const WatchlistContentFilters: React.FC<{ filters: FilterArgs }> = ({ fil
       />
 
       {(filters.type || filters.sort) && (
-        <button onClick={filters.clearFilters} className="btn-ghost btn-sm btn gap-2 p-0.5">
-          Clear Filters
+        <button onClick={filters.clearFilters} className="btn-ghost btn-sm btn ml-4 gap-2 p-0.5">
+          {isMd ? 'Clear Filters' : 'Clear'}
         </button>
       )}
     </div>
