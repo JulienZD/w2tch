ac2624d091f911503ec773ae94de61961e31e638
Julien Zapata Duque <jzapata@live.nl>
2023-01-31 20:03:09 +0100

    fix: don't fetch watchlist on SSR for client-side nav
    This causes a noticeable delay in production environments,
    we do still want to perform SSR to properly show opengraph
    information

--- src/pages/watchlist/[id].tsx
+++ src/pages/watchlist/[id].tsx
@@ -3,21 +3,22 @@ import { UserPlusIcon } from '@heroicons/react/24/solid';
 import { TRPCError } from '@trpc/server';
 import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next';
 import pluralize from 'pluralize';
+import { SEO } from '~/components/common/SEO';
 import { AddItem } from '~/components/features/watchlist/AddItem';
 import { InviteModal } from '~/components/features/watchlist/invite/InviteModal';
 import { WatchlistContent } from '~/components/features/watchlist/WatchListContent';
 import { WatchlistContentFilters } from '~/components/features/watchlist/WatchlistContentFilters';
 import { WatchlistOverflowMenu } from '~/components/features/watchlist/WatchlistOverflowMenu';
 import { Pluralize } from '~/components/util/Pluralize';
-import { env } from '~/env/server.mjs';
+import { env } from '~/env/client.mjs';
 import { useFilterWatchlistEntries } from '~/hooks/watchlist/useFilterWatchlistEntries';
 import type { WithSEOProps } from '~/types/ssr';
-import { api } from '~/utils/api';
+import { api, type RouterOutputs } from '~/utils/api';
 import { toPossessive } from '~/utils/language';
 import { optionalSeo } from '~/utils/seo';
-import { createSSGHelper } from '~/utils/ssg';
+import { createSSGHelper, isLoadedViaClientNavigation } from '~/utils/ssg';
 
-const WatchList: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ watchlistId }) => {
+const WatchList: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ watchlistId, skippedSSR }) => {
   const { data: watchlist } = api.watchlist.byId.useQuery({ id: watchlistId });
   const { filteredWatchables, ...filters } = useFilterWatchlistEntries(watchlist?.watchables ?? []);
 
@@ -34,65 +35,79 @@ const WatchList: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>
   const readOnly = watchlist.isReadOnly;
 
   return (
-    <div className="prose max-w-full">
-      <div className="flex flex-col gap-x-0">
-        <span>{watchlist.isVisibleToPublic ? 'Public' : 'Private'} watchlist</span>
-        <h1 className="mt-0 pt-0">{watchlist.name}</h1>
-      </div>
-      <div className="flex flex-col items-start justify-between gap-x-4 text-sm md:flex-row md:items-center md:justify-start">
-        <span>
-          List by <span className="font-semibold">{watchlist.owner?.name}</span>
-        </span>
-        <div className="my-0 flex flex-row items-center gap-y-0 gap-x-2 max-md:w-full max-md:justify-between md:before:-ml-2 md:before:content-['•']">
-          <div className="flex items-center gap-x-2">
-            <div className="flex items-center">
-              <span className="whitespace-nowrap">
-                <Pluralize count={watchlist.memberCount} word="member" />
-              </span>
-              {watchlist.isOwner && (
-                <span data-tip="Invite People" className="tooltip tooltip-top">
-                  <button
-                    onClick={openInviteModal}
-                    aria-label="Invite People"
-                    className="btn-ghost btn-sm btn mx-0.5 px-1"
-                  >
-                    <UserPlusIcon className="h-5 w-5" />
-                  </button>
-                </span>
-              )}
-            </div>
-            <span className="whitespace-nowrap">
-              <Pluralize count={watchlist.watchableCount} word="entry" />
-            </span>
-          </div>
-          {!readOnly && watchlist.isOwner && <WatchlistOverflowMenu watchlistId={watchlist.id} />}
+    <>
+      {skippedSSR && <SEO {...getWatchlistSeo(watchlist)} />}
+      <div className="prose max-w-full">
+        <div className="flex flex-col gap-x-0">
+          <span>{watchlist.isVisibleToPublic ? 'Public' : 'Private'} watchlist</span>
+          <h1 className="mt-0 pt-0">{watchlist.name}</h1>
         </div>
+        <div className="flex flex-col items-start justify-between gap-x-4 text-sm md:flex-row md:items-center md:justify-start">
+          <span>
+            List by <span className="font-semibold">{watchlist.owner?.name}</span>
+          </span>
+          <div className="my-0 flex flex-row items-center gap-y-0 gap-x-2 max-md:w-full max-md:justify-between md:before:-ml-2 md:before:content-['•']">
+            <div className="flex items-center gap-x-2">
+              <div className="flex items-center">
+                <span className="whitespace-nowrap">
+                  <Pluralize count={watchlist.memberCount} word="member" />
+                </span>
+                {watchlist.isOwner && (
+                  <span data-tip="Invite People" className="tooltip tooltip-top">
+                    <button
+                      onClick={openInviteModal}
+                      aria-label="Invite People"
+                      className="btn-ghost btn-sm btn mx-0.5 px-1"
+                    >
+                      <UserPlusIcon className="h-5 w-5" />
+                    </button>
+                  </span>
+                )}
+              </div>
+              <span className="whitespace-nowrap">
+                <Pluralize count={watchlist.watchableCount} word="entry" />
+              </span>
+            </div>
+            {!readOnly && watchlist.isOwner && <WatchlistOverflowMenu watchlistId={watchlist.id} />}
+          </div>
+        </div>
+
+        {!readOnly && <AddItem watchlistId={watchlist.id} />}
+
+        {!!watchlist.watchables.length && <WatchlistContentFilters filters={filters} />}
+
+        <div className="divider mt-0.5" />
+
+        <WatchlistContent
+          readOnly={readOnly}
+          isOwner={watchlist.isOwner}
+          items={filteredWatchables}
+          watchlistId={watchlistId}
+        />
       </div>
-
-      {!readOnly && <AddItem watchlistId={watchlist.id} />}
-
-      {!!watchlist.watchables.length && <WatchlistContentFilters filters={filters} />}
-
-      <div className="divider mt-0.5" />
-
-      <WatchlistContent
-        readOnly={readOnly}
-        isOwner={watchlist.isOwner}
-        items={filteredWatchables}
-        watchlistId={watchlistId}
-      />
-    </div>
+    </>
   );
 };
 
 export default WatchList;
 
-export const getServerSideProps: GetServerSideProps<WithSEOProps<{ watchlistId: string }, true>> = async (ctx) => {
+export const getServerSideProps: GetServerSideProps<
+  WithSEOProps<{ watchlistId: string; skippedSSR?: true }, true>
+> = async (ctx) => {
   const watchlistId = ctx.params?.id as string | undefined;
 
   if (!watchlistId) {
     return {
       notFound: true,
+    };
+  }
+
+  if (isLoadedViaClientNavigation(ctx.req)) {
+    return {
+      props: {
+        skippedSSR: true,
+        watchlistId,
+      },
     };
   }
 
@@ -116,14 +131,16 @@ export const getServerSideProps: GetServerSideProps<WithSEOProps<{ watchlistId: 
     props: {
       trpcState: ssg.dehydrate(),
       watchlistId,
-      ...optionalSeo(watchlist.isVisibleToPublic, {
-        title: watchlist.name,
-        description: `
-            See ${toPossessive(watchlist.owner?.name ?? 'unknown')} watchlist on ${env.NEXT_PUBLIC_APP_NAME} | ${
-          watchlist.watchableCount
-        } ${pluralize('title', watchlist.watchableCount)}
-        `.trim(),
-      }),
+      ...optionalSeo(watchlist.isVisibleToPublic, getWatchlistSeo(watchlist)),
     },
   };
 };
+
+const getWatchlistSeo = (watchlist: RouterOutputs['watchlist']['byId']) => ({
+  title: watchlist.name,
+  description: `
+    See ${toPossessive(watchlist.owner?.name ?? 'unknown')} watchlist on ${env.NEXT_PUBLIC_APP_NAME} | ${
+    watchlist.watchableCount
+  } ${pluralize('title', watchlist.watchableCount)}
+  `.trim(),
+});
--- src/utils/ssg.ts
+++ src/utils/ssg.ts
@@ -4,6 +4,7 @@ import { createContextInner } from '~/server/trpc/context';
 import { createProxySSGHelpers } from '@trpc/react-query/ssg';
 import { appRouter } from '~/server/trpc/router/_app';
 import superjson from 'superjson';
+import type { IncomingMessage } from 'http';
 
 export const createSSGHelper = async (nextCtx: GetServerSidePropsContext) => {
   const session = await getServerAuthSession(nextCtx);
@@ -18,4 +19,13 @@ export const createSSGHelper = async (nextCtx: GetServerSidePropsContext) => {
     ctx,
     session,
   };
+};
+
+/**
+ * Whether the request is a client navigation request
+ *
+ * This can be used to determine whether to continue with performing operations in getServerSideProps
+ */
+export const isLoadedViaClientNavigation = (req: IncomingMessage) => {
+  return req.url?.indexOf('/_next/data/') !== -1;
 };
